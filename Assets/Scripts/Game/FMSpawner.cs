cdccd21dacca2e084b4d94a0feab957dd86a654b
Frictionpowered <denes@nagymathe.com>
2019-09-21 21:09:01 +0100

    spawning ;)

---
+++ Assets/Scripts/Game/FMSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public Transform parent;
+    public int numberToSpawn = 10;
+
+    public Vector3 randomPosRange = new Vector3(5, 0, 5);
+
+    void Start()
+    {
+        for(int i=0; i<numberToSpawn; i++)
+        {
+            Vector3 offset = Vector3.Scale(Random.insideUnitSphere, randomPosRange);
+            GameObject go = Instantiate(prefab, parent.position + offset, parent.rotation, parent);
+            go.name = "spawn" + i;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
