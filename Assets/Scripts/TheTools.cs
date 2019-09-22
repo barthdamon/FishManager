d10c584a0f405bb2721dd2cf0f9bfbeb2687b4c5
Frictionpowered <denes@nagymathe.com>
2019-09-22 09:26:44 +0100

    LetThereBeLight! and shadows.

---
+++ Assets/Scripts/TheTools.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheTools : MonoBehaviour
+{
+    [InspectorButton("FindSprites", "SetThemUp")]
+    public bool _;
+
+    public Material material;
+    public List<SpriteRenderer> sprites;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FindSprites()
+    {
+        sprites.AddRange(GetComponentsInChildren<SpriteRenderer>());
+    }
+
+    void SetThemUp()
+    {
+        foreach(SpriteRenderer sr in sprites)
+        {
+            if (sr.material != material)
+            {
+                sr.material = material;
+
+                sr.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                sr.receiveShadows = true;
+            }
+        }
+    }
+}
